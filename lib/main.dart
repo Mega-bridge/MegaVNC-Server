c44128f7e7c642a5d6236e83316ec31658e8f559
jongwonyang <yjw963@gmail.com>
2023-12-14 11:28:47 +0900

    https 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:megavnc_server/config.dart';
+import 'package:megavnc_server/http_override.dart';
 import 'package:megavnc_server/uvnc_ini.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
@@ -26,6 +27,8 @@ void main() async {
     await windowManager.show();
     await windowManager.focus();
   });
+
+  HttpOverrides.global = DevHttpOverrides();
 
   runApp(const MyApp());
 }
